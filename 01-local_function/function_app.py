071a59e8a4f5847462ecb74c3d3c1807c554ca53
mohamed eraki <mohamed-ibrahim2021@outlook.com>
2025-05-16 20:22:22 +0300

    Add functionality to process student requests in localHTTPfunc

--- 01-local_function/function_app.py
+++ 01-local_function/function_app.py
@@ -2,26 +2,33 @@ import azure.functions as func
 import datetime
 import json
 import logging
+import pandas as pd
 
 app = func.FunctionApp()
 
-@app.route(route="localHTTPfunc", auth_level=func.AuthLevel.ANONYMOUS)
+
+# @app.route(route="localHTTPfunc", auth_level=func.AuthLevel.ANONYMOUS)
+@app.route(route="students_req", auth_level=func.AuthLevel.ANONYMOUS)
 def localHTTPfunc(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    try:
+        req_body = req.get_json()  # use postman to send json body
+        logging.info(req_body)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
+    except ValueError:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "Json body requirement for this function to work!", status_code=500
         )
+    
+    df = pd.DataFrame(req_body['students'])
+    df = df.explode('courses').reset_index(drop=True)
+    df = pd.merge(df, pd.json_normalize(df['courses']), left_index=True, right_index=True)
+    res = df.to_csv()
+
+    return func.HttpResponse(
+        # req_body,
+        # f"Successfully loaded the json body! {df}",
+        res,
+        status_code=200,
+    )
